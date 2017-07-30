1261374dd3a48c011041d4597ddcdeed64d8a774
ckingbailey <ckingbailey@gmail.com>
2017-07-29 23:17:34 -0700

    timer operational but inaccurate

--- mockup/js/clock.js
+++ mockup/js/clock.js
@@ -1,14 +1,17 @@
 var date = new Date(),
   hrs, mins, time,
-  clockEl = document.querySelector('.time');
+  clockEl = document.querySelector('.time'),
+  timerForm = document.forms['set-timer'];
+
+timerForm.addEventListener('submit', function(ev) {
+  ev.preventDefault();
+  setTimer(timerForm['timer-cal'].value);
+});
 
 //fcn to get new hr:min
 //and post it to timeEl
 //to be fired with setInterval once a minute
 function updateClock() {
-//what's the best way to get new hr:min?
-//define a new Date object every minute?
-//or store orig hr:min values and incr them myself each min?
   date = new Date();
   //it would be good if date were private
   hrs = date.getHours() > 12 ? date.getHours() - 12 : date.getHours() !== 0 ? date.getHours() : 12;
@@ -23,11 +26,15 @@ function setTimer(end) {
   //'2017-07-29T17:00'
   //if I use a "datetime-local" input
   //this will have to be parsed with regex to something Date() can accept
-  var [y, mo, d, h, m] = end.match(/^(\d{n})(?:-)(\d{2})(?:-)(\d{2})(?:T)(\d{2})(?::)(\d{2})/).shift(),
-  endTime = new Date(y, mo, d, h, m),
+  var splitEnd = end.match(/^(\d{4})(?:-)(\d{2})(?:-)(\d{2})(?:T)(\d{2})(?::)(\d{2})/),
+  endTime = new Date(splitEnd[1], splitEnd[2] - 1, splitEnd[3], splitEnd[4], splitEnd[5]),
   curTime = date;
   sRmn = Math.round((endTime.valueOf() - curTime.valueOf())/1000),
-  timeLeft = {};
+  timeLeft = {},
+  displayCt = document.querySelector('.countdown'),
+  ctStr = '',
+  intID = window.setInterval(updateTimer, 1000);
+  console.log(curTime, endTime, sRmn);
 
   function countDown() {
     var totRmn;
@@ -43,8 +50,18 @@ function setTimer(end) {
 
   function updateTimer() {
     countDown();
+    ctStr = '';
+    ctStr += timeLeft.d ? timeLeft.d + ':' : '';
+    ctStr += timeLeft.h ? timeLeft.h + ':' : '';
+    ctStr += timeLeft.m ? timeLeft.m + ':' : '';
+    ctStr += timeLeft.s ? timeLeft.s : '';
+    if(!ctStr) {
+      displayCt.innerHTML = "time's up!";
+      window.clearInterval(intID);
+    }
+    else displayCt.innerHTML = ctStr;
   }
 }
 
-//updateClock();
-//window.setInterval(updateClock, 60000);
+updateClock();
+window.setInterval(updateClock, 60000);
