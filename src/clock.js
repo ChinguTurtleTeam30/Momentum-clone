e9cf4184e2d6f920076618e3db5d0fb07865748b
ckingbailey <ckingbailey@gmail.com>
2017-08-05 23:11:45 -0700

    clock renders properly, no updateTime() yet

--- src/clock.js
+++ src/clock.js
@@ -1,39 +1,39 @@
 import React, { Component } from 'react';
 
-class Clock extends React.Component {
+class Clock extends Component {
   renderTime() {
     const date = new Date();
     function renderHours(time) {
       return (
-        time.getHours() > 12 ? (time.getHours() - 12).toString() : time.getHours() !== 0 ? time.getHours().toString() : '12';
+        time.getHours() > 12 ? (time.getHours() - 12).toString() : time.getHours() !== 0 ? time.getHours().toString() : '12'
       );
     }
     function renderMins(time) {
       return (
-        time.getMinutes() < 10 ? '0' + time.getMinutes() : time.getMinutes().toString();
+        time.getMinutes() < 10 ? '0' + time.getMinutes() : time.getMinutes().toString()
       );
     }
     function renderSign(time) {
       return (
-        time.getHours() >= 12 ? 'pm' : 'am';
+        time.getHours() >= 12 ? 'pm' : 'am'
       );
     }
     return (
-      renderHours(date) + renderMins(date) + renderSign(date);
+      renderHours(date) + ':' + renderMins(date) + ' ' + renderSign(date)
     );
   }
   render() {
     return (
       <div className="clock">
         <p className="time">
-          { this.renderHours(); }
+          { this.renderTime() }
         </p>
       </div>
     )
   }
 }
 
-class Timer extends React.Component {
+class Timer extends Component {
   render() {
     return (
       <div class="timer main-flex-item">
