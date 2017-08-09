053eb4e52ba108b75ca1b439d23710f60936b5b0
ckingbailey <ckingbailey@gmail.com>
2017-08-08 17:26:52 -0700

    make state for Timer

--- src/timer.js
+++ src/timer.js
@@ -1,11 +1,19 @@
 import React, { Component }, from 'react';
 
 class Timer extends Component {
+  constructor() {
+    super();
+    this.state = {
+      countdown: 0,
+      endTime: new Date(),
+    };
+  }
+  
   render() {
     <div className="timer">
       <p className="countdown-display"></p>
       <form className="timer-form">
-        <input id="select-end-time" name="select-end-time" type="datetime-local"/>
+        <input id="select-end-time" name="select-end-time" type="datetime-local" defaultValue={ this.state.endTime }/>
         <input id="start-timer" name="start-timer" type="submit"/>
         <input id="stop-timer" name="stop-timer" type="button"/>
         <input id="reset-timer" name="reset-timer" type="button"/>
