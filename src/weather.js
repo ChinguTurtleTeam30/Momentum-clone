5bf7bc89e5eb2666fd191070c54acb1336551aaa
ckingbailey <ckingbailey@gmail.com>
2017-08-15 22:24:35 -0700

    nav.geo not working in React

--- src/weather.js
+++ src/weather.js
@@ -6,8 +6,47 @@ class Weather extends Component {
     this.state = {
       temperature: null,
       location: null,
+      lat: null,
+      lon: null,
       weatherType: null,
+      apiKey: 'APPID=79aef489883f75aff91f8900796eb1ea',
     }
+  }
+
+  qryParams = []
+
+  getLoc = (obj) => {
+    if ('geolocation' in navigator) {
+      navigator.geolocation.getCurrentPosition(
+        function(pos) {
+          console.log('001', pos.coords);
+          return (
+            obj.push(pos.coords.latitude.toFixed(2),
+              pos.coords.longitude.toFixed(2)),
+            console.log('002', pos.coords.latitude, pos.coords.longitude)
+          );
+        },
+        function(err) {
+          return (
+            console.error(err),
+            alert(err)
+          );
+        }
+      );
+      console.log('003', obj);
+      this.setState({ lat: 1, lon: 2 });
+    }
+    else {
+      let noGeo = new Error(Error('navigator.geolocation unavailable'));
+      return (
+        console.error(noGeo),
+        alert(noGeo)
+      );
+    }
+  }
+
+  componentWillMount() {
+    this.getLoc(this.qryParams);
   }
 
   render() {
