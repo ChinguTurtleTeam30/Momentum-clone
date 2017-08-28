782ec83cbcb9478a5f8de5cd51465bd7f4d66c72
dmatt <dmatt@disqus.com>
2017-08-27 19:22:31 -0700

    Fetch access_token and render background image from artsy API
    Makes 2 fetch requests, 1 to get the access token and stores that in the browser localStorage for next time and 1 request to get some actual random art with the /artworks?&sample=1 resource which grabs a random element in the JSON reponse. Right now, this component renders a backup image from Imgur when there is no artsy access token. That needs to be improved.

--- src/art.js
+++ src/art.js
@@ -1,28 +1,57 @@
 import React, { Component } from 'react';
 
+// Initial variables for API configuration
+var clientID = '7c49150d5697e33be871',
+    clientSecret = process.env.REACT_APP_CLIENTSECRET,
+    apiUrl = 'https://api.artsy.net/api/tokens/xapp_token',
+    resourceUrl = 'https://api.artsy.net/api/artworks?&sample=1',
+    artsyToken = localStorage.getItem('artsyToken') || null,
+    appKeys = JSON.stringify({ client_id: clientID, client_secret: clientSecret }),
+    artsyRandomArtUrl;
+
+// Get artsy access token and set in localStorage for next pageload
+fetch(apiUrl, {method: 'POST', body: appKeys}).then(function(response) {
+	return response.json();
+}).then(function(data) {
+	artsyToken = data.token;
+	localStorage.setItem('artsyToken', artsyToken);
+});
+
 class Art extends Component {
 
 	constructor(props) {
 		super(props);
-		this.state = { img: null };
+		this.state = { img: null, artsyToken: null };
 	}
 
 	componentDidMount() {
-		fetch('https://i.imgur.com/A2osoec.jpg').then(function(response) {
-			return response.blob();
-		}).then(function(response) {
-			var objectURL = URL.createObjectURL(response);
-			console.log(objectURL);
-			this.setState( {img: objectURL});
-			console.log(objectURL, this.state);
-		}.bind(this));
+		if (artsyToken) {
+			// Get a random artsy image
+			fetch(resourceUrl, {method: 'GET', headers: {
+					'X-Xapp-Token': artsyToken,
+					'Accept': 'application/vnd.artsy-v2+json'
+				}}).then(function(response) {
+					return response.json();
+				}).then(function(data) {
+					console.log("WOW arts",data);
+					artsyRandomArtUrl = data._links.image.href.replace('{image_version}','large');
+					this.setState ( {img: artsyRandomArtUrl, artsyToken: artsyToken} );
+				}.bind(this));
+		} else {
+			// fetch imgur image as a backup
+			fetch('https://i.imgur.com/A2osoec.jpg').then(function(response) {
+				return response.blob();
+			}).then(function(response) {
+				var objectURL = URL.createObjectURL(response);
+				this.setState( {img: objectURL});
+			}.bind(this));
+		}
 	}
 
 	render() {
 		if (this.state.img) {
 			return <div id="art" className="art overlay" style={{backgroundImage: 'url('+ this.state.img + ')'}} />
 		}
-
 		return <div>Getting art...</div>;
 	}
 };
