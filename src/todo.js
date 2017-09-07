bbc20dfee36945ec51a74edaba9ed88791eb355e
ckingbailey <ckingbailey@gmail.com>
2017-09-06 19:56:40 -0700

    click and submit handlers for Todo

--- src/todo.js
+++ src/todo.js
@@ -2,11 +2,22 @@ import React, { Component } from 'react';
 import './todo.css';
 import './font-awesome/css/font-awesome.min.css';
 
-function TodoPanel() {
+function TodoPanel(props) {
   return (
     <div className="panel todoPanel">
-      <input id="todoTextInput" name="todoTextInput" type="text" />
-      <ul id="todoList" className="todoList"></ul>
+      <form id="todoForm" name="todoForm" onSubmit={ (event) => props.handleSubmit(event) }>
+        <input id="todoTextInput"
+              name="todoTextInput"
+              type="text"
+        />
+      </form>
+      <ul id="todoList" className="todoList">
+        {
+          props.todoItems.map(function(item, i) {
+            return <li className="todoItem" key={ "todoItem" + i }>{ item }</li>
+          })
+        }
+      </ul>
     </div>
   );
 }
@@ -16,14 +27,41 @@ export default class Todo extends Component {
     super(props);
     this.state = {
       todoPanelOpen: false,
+      todo: [],
     }
   }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    if (event.target.name === 'todoForm') {
+      const todoItem = event.target['todoTextInput'].value;
+      //console.log(todoItem);
+      this.setState((prevState) => {
+        return prevState.todo.push(todoItem);
+      })
+    }
+  }
+
+  handleClick(event) {
+    if (event.target.id.includes('togglePanel')) {
+      this.setState({ todoPanelOpen: !this.state.todoPanelOpen });
+    }
+  }
+
   render() {
     return (
       <div className="todo">
         { this.state.todoPanelOpen ?
-        <TodoPanel /> : null }
-        <i className="fa fa-list-ul toggleTodoPanelOpen" tabIndex="0"></i>
+          <TodoPanel handleSubmit={ (event) => this.handleSubmit(event) }
+                    todoItems={ this.state.todo }
+          /> :
+          null
+        }
+        <i className="fa fa-list-ul togglePanelTodo"
+          id="togglePanelTodo"
+          tabIndex="0"
+          onClick={ (event) => this.handleClick(event) }
+        ></i>
       </div>
     );
   }
