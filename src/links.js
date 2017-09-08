e6d2754f9615d18989306fc7db7c054f213f7324
ckingbailey <ckingbailey@gmail.com>
2017-09-08 12:02:30 -0700

    make linksPanel and click handler to open/close it

--- src/links.js
+++ src/links.js
@@ -2,10 +2,52 @@ import React, { Component} from 'react';
 import './links.css';
 import './font-awesome/css/font-awesome.min.css';
 
+function LinksPanel(props) {
+  return (
+    <div className="panel linksPanel">
+      <p>New blank tab</p>
+      <ul className="chromeLinks">
+        <li>Apps</li>
+      </ul>
+      <ul className="bookmarks"></ul>
+      <ul className="mostVistied"></ul>
+      <ul className="recentlyVisited"></ul>
+    </div>
+  )
+}
+
 export default class Links extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      linksPanelOpen: false,
+    }
+  }
+
+  handleClick(event) {
+    if (event.target.id.includes('togglePanelOpen')) {
+      const key = event.target.parentElement.id + 'PanelOpen';
+      this.setState((prevState) => {
+        console.log(key, prevState);
+        return { [key]: !prevState[key] };
+      });
+    }
+    else return;
+  }
+
   render() {
     return (
-      <i className="fa fa-th" tabIndex='0'></i>
+      <div id="links" className="links">
+        <i className="fa fa-th togglePanelOpenLinks"
+          id="togglePanelOpenLinks"
+          tabIndex='0'
+          onClick={ (event) => this.handleClick(event) }
+        ></i>
+        { this.state.linksPanelOpen ?
+          <LinksPanel /> :
+          null
+        }
+      </div>
     );
   }
 }
