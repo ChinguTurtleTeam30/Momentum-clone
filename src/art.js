9c8e0e34c1ac9cc7ffc2569dd33f0baecb611937
ckingbailey <ckingbailey@gmail.com>
2017-09-12 16:24:18 -0700

    new fcn in cb pattern to fetch token

--- src/art.js
+++ src/art.js
@@ -1,52 +1,109 @@
 import React, { Component } from 'react';
 import './art.css';
 
-// Initial variables for API configuration
-var clientID = '7c49150d5697e33be871',
-    clientSecret = process.env.REACT_APP_CLIENTSECRET || '204d8604bbc71c2038192655565f01f8',
-    apiUrl = 'https://api.artsy.net/api/tokens/xapp_token',
-    resourceUrl = 'https://api.artsy.net/api/artworks?&sample=1',
-    artsyToken = localStorage.getItem('artsyToken') || null,
-    appKeys = JSON.stringify({ client_id: clientID, client_secret: clientSecret }),
-    artsyRandomArtUrl;
-
-// Get artsy access token and set in localStorage for next pageload
-fetch(apiUrl, {method: 'POST', body: appKeys}).then(function(response) {
-	return response.json();
-}).then(function(data) {
-	artsyToken = data.token;
-	localStorage.setItem('artsyToken', artsyToken);
-});
-
 export default class Art extends Component {
 
 	constructor(props) {
 		super(props);
-		this.state = { img: null, artsyToken: null };
+		this.state = {
+      img: null,
+      artsyToken: null
+    };
 	}
 
+  artsyStaticData = {
+    tokenReq: {
+      clientID: '7c49150d5697e33be871',
+      key: process.env.REACT_APP_CLIENTSECRET || '204d8604bbc71c2038192655565f01f8',
+      url: 'https://api.artsy.net/api/tokens/xapp_token'
+    },
+    artReq: {
+      url: 'https://api.artsy.net/api/artworks?&sample=1',
+      token: window.localStorage.getItem('artsyToken') || null
+    }
+  }
+
+  getNewToken(reqObj, callback) {
+    const options = {
+      method: 'POST',
+      body: JSON.stringify({
+        client_id: reqObj.clientID,
+        client_secret: reqObj.key
+      })
+    };
+
+    fetch(reqObj.url, options)
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      return callback(data);
+    });
+  }
+
+  getNewArt(reqObj, callback) {
+    const options = {
+      method: 'GET',
+      headers: {
+        'X-Xapp-Token': reqObj.token,
+        Accept: 'application/vnd.artsy-v2+json'
+      }
+    }
+
+    fetch(reqObj.url, options)
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      return callback(data);
+    });
+  }
+
 	componentDidMount() {
-		if (artsyToken) {
-			// Get a random artsy image
-			fetch(resourceUrl, {method: 'GET', headers: {
-					'X-Xapp-Token': artsyToken,
-					'Accept': 'application/vnd.artsy-v2+json'
-				}}).then(function(response) {
-					return response.json();
-				}).then(function(data) {
-					//console.log("WOW arts",data);
-					artsyRandomArtUrl = data._links.image.href.replace('{image_version}','large');
-					this.setState ( {img: artsyRandomArtUrl, artsyToken: artsyToken} );
-				}.bind(this));
-		} else {
-			// fetch imgur image as a backup
-			fetch('https://i.imgur.com/A2osoec.jpg').then(function(response) {
-				return response.blob();
-			}).then(function(response) {
-				var objectURL = URL.createObjectURL(response);
-				this.setState( {img: objectURL});
-			}.bind(this));
-		}
+    let now = new Date(),
+		    artExpiry = window.localStorage.getItem('artExpiry') &&
+	        new Date(window.localStorage.getItem('artExpiry')) > now ?
+					new Date(window.localStorage.getItem('artExpiry')) :
+					new Date(now.getFullYear(), now.getMonth(), now.getDate() + 1),
+        tokenExpiry = window.localStorage.getItem('artsyTokenExpiry') &&
+          new Date(window.localStorage.getItem('artsyTokenExpiry')) > now ?
+					new Date(window.localStorage.getItem('artsyTokenExpiry')) : null;
+
+    //if art is in localStorage and it's fresh, load it up
+    if (window.localStorage['bgImg'] && artExpiry > now) {
+      return this.setState({ img: window.localStorage.getItem('bgImg') });
+    }
+    //if access token is fresh, get new art
+    else if (tokenExpiry > now) {
+      this.getNewArt(this.artsyStaticData.artReq, (data) => {
+				const newArt = data._links.image.href.replace('{image_version}','large');
+        this.props.store({ artExpiry: artExpiry, bgImg: newArt });
+        return (
+          this.setState({
+            img: data._links.image.href.replace('{image_version}','large')
+          })
+        );
+      });
+    }
+    //if art and token are both stale, get new token and art
+		else {
+	    this.getNewArt({
+        token: this.getNewToken(this.artsyStaticData.tokenReq, (data) => {
+          this.props.store({ artsyToken: data.token, artsyTokenExpiry: data.expires_at });
+          return data.token;
+        }),
+        url: this.artsyStaticData.artReq.url
+        }, (data) => {
+          const newArt = data._links.image.href.replace('{image_version}','large');
+          this.props.store({ artExpiry: artExpiry, bgImg: newArt });
+          return (
+            this.setState({
+              img: newArt
+            })
+          );
+        }
+      );
+    }
 	}
 
 	render() {
