a40b67ed3dbcab6a4e026262938066593bc09846
ckingbailey <ckingbailey@gmail.com>
2017-12-21 13:13:47 -0800

    move css files into css folder

--- src/links.js
+++ src/links.js
@@ -26,33 +26,33 @@ function LinksFolder(props) {
 
 //LinksPanel should handle the state for what is open|closed
 function LinksPanel(props) {
-  const apps = 'chrome://apps',
-        extensions = 'chrome://extensions',
-        chrome = window.chrome,
-        bookmarks = chrome.bookmarks;
+  const apps = 'chrome://apps';
+  //       extensions = 'chrome://extensions',
+  const chrome = window.chrome;
+  //       bookmarks = chrome.bookmarks;
 
 //I ought to traverseTree on load and not on each render
 //this should run on compDidMt
-  const traverseBookmarkTree = (id) => {
-    //will return a var containing a tree of ul & li
-    const tree = [];
-    bookmarks.getTree((nodeTree) => {
-      tree = nodeTree;
-    });
-
-    //is this thing gonna be recursive?
-    bookmarks.getChildren(i, (childNodes) {
-      if (!childNodes) {
-        return tree.push(<Bookmark />);
-      }
-      else {
-        //no, this needs to be recursive, too
-        tree.push(<LinksFolder />);
-        //how do I pass different index nums to this fcn?
-        traverseBookmarkTree(childNodes[0].id);
-      }
-    }
-  }
+  // const traverseBookmarkTree = (id) => {
+  //   //will return a var containing a tree of ul & li
+  //   let tree = [];
+  //   bookmarks.getTree((nodeTree) => {
+  //     tree = nodeTree;
+  //   });
+  //
+  //   //is this thing gonna be recursive?
+  //   bookmarks.getChildren(i, (childNodes) => {
+  //     if (!childNodes) {
+  //       return tree.push(<Bookmark />);
+  //     }
+  //     else {
+  //       //no, this needs to be recursive, too
+  //       tree.push(<LinksFolder />);
+  //       //how do I pass different index nums to this fcn?
+  //       traverseBookmarkTree(childNodes[0].id);
+  //     }
+  //   });
+  // }
 
 //LinksPanel should own the traverseBMs fcn, to be run by one LinksFolder
   return (
@@ -100,26 +100,26 @@ export default class Links extends Component {
     else return;
   }
 
-  traverseBookmarkTree(bookmarkNodeID) {
-    const tree = this.renderBookmarksList(0);
-    // get, getTree, getChildren
-    // title, url
-    // because chrome.bookmarks methods have no return value
-    // it would be easiest if I wrap them in a fcn that does return sthing
-    chrome.bookmarks.getChildren(id, (children) => {
-      if (children) {
-      }
-      // because getChildren has no return value
-      // I need to feed this var to a function that returns sthing
-      else {
-        (id) => chrome.bookmarks.get(id, (bookmark) => {
-          // the return value of this render fcn will not get returned
-          // I need to pass the return value into an existing outside var
-          tree.appendChild(this.renderBookmark(bookmark));
-        })
-      }
-    });
-  }
+  // traverseBookmarkTree(bookmarkNodeID) {
+  //   const tree = this.renderBookmarksList(0);
+  //   // get, getTree, getChildren
+  //   // title, url
+  //   // because chrome.bookmarks methods have no return value
+  //   // it would be easiest if I wrap them in a fcn that does return sthing
+  //   chrome.bookmarks.getChildren(id, (children) => {
+  //     if (children) {
+  //     }
+  //     // because getChildren has no return value
+  //     // I need to feed this var to a function that returns sthing
+  //     else {
+  //       (id) => chrome.bookmarks.get(id, (bookmark) => {
+  //         // the return value of this render fcn will not get returned
+  //         // I need to pass the return value into an existing outside var
+  //         tree.appendChild(this.renderBookmark(bookmark));
+  //       })
+  //     }
+  //   });
+  // }
 
   render() {
     return (
